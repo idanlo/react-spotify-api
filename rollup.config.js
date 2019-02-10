d49a9042bedcd37e1e2aa43e65a7868a8f83806d
idanlo <idan2110@gmail.com>
2019-02-10 15:01:17 +0200

    update to babel 7 and react-scripts 2.1.3, all tests work

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+import url from 'rollup-plugin-url';
+import svgr from '@svgr/rollup';
 
-import pkg from './package.json'
+import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
@@ -26,12 +27,13 @@ export default {
     postcss({
       modules: true
     }),
-    url(),
+    url({ exclude: ['**/*.svg'] }),
+    svgr(),
     babel({
       exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
+      plugins: ['@babel/external-helpers']
     }),
     resolve(),
     commonjs()
   ]
-}
+};
