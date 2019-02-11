f5f55fd9b67853c59d9d1eb276a4947be4916fbc
idanlo <idan2110@gmail.com>
2019-02-11 19:43:23 +0200

    new hook useTrack which is very similiar to useArtist, only different api endpoint

---
+++ src/lib/Track/useTrack.js
@@ -0,0 +1,13 @@
+import useApiRequest from '../ApiRequest/useApiRequest';
+
+const BASE_URL = 'https://api.spotify.com/v1/tracks';
+
+function useTrack(id) {
+    const url = Array.isArray(id) ? BASE_URL : BASE_URL + `/${id}`;
+    const options = Array.isArray(id) ? { ids: id.join(',') } : {};
+    const { data, loading, error } = useApiRequest(url, options);
+
+    return { data, loading, error };
+}
+
+export default useTrack;
