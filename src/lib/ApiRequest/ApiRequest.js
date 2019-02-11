b73a75a32af1703cb0549860943ea691dcbbbfe6
idanlo <idan2110@gmail.com>
2019-02-11 18:57:07 +0200

    rewrite ApiRequest component completely, it now uses the useApiRequest hook and is very short. it is only used to pass the render props

--- src/lib/ApiRequest/ApiRequest.js
+++ src/lib/ApiRequest/ApiRequest.js
@@ -1,31 +1,8 @@
-import React from 'react';
-import { SpotifyApiContext } from '../';
 import PropTypes from 'prop-types';
-import axios from 'axios';
+import useApiRequest from './useApiRequest';
 
 function ApiRequest(props) {
-    const [data, setData] = React.useState(null);
-    const [loading, setLoading] = React.useState(true);
-    const [error, setError] = React.useState(false);
-    const token = React.useContext(SpotifyApiContext);
-    React.useEffect(() => {
-        console.log('REACT-SPOTIFY-API HOOKS UPDATE - FETCHING DATA');
-        axios
-            .get(props.url, {
-                params: props.options,
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            })
-            .then(res => {
-                setData(res.data);
-                setLoading(false);
-            })
-            .catch(err => {
-                console.log(err);
-                setError(true);
-            });
-    }, [props.url, props.options]);
+    const { data, loading, error } = useApiRequest(props.url, props.options);
     return props.children(data, loading, error);
 }
 
