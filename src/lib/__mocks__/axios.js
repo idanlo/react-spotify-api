44cb6b5172671572f93b7087d21dd63ca176aa2b
idanlo <idan2110@gmail.com>
2019-02-14 09:09:48 +0200

    add axios mocks

---
+++ src/lib/__mocks__/axios.js
@@ -0,0 +1,17 @@
+module.exports = {
+    get: jest.fn((url, opts) => {
+        return new Promise((resolve, reject) => {
+            if (url) {
+                if (url === 'https://test.com/ApiRequest') {
+                    resolve({
+                        data: {
+                            url
+                        }
+                    });
+                }
+            } else {
+                reject({ message: 'Error' });
+            }
+        });
+    })
+};
