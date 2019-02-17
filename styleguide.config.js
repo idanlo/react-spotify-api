51da298ba35713e5642fe8d28bf601896d8bf2ca
idanlo <idan2110@gmail.com>
2019-02-17 18:51:28 +0200

    docs for artist components

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,13 +2,13 @@ module.exports = {
     webpackConfig: require('react-scripts/config/webpack.config.js')(
         'development'
     ),
+    // components: 'src/lib/**/*.js'
     sections: [
         {
             name: 'Artist components',
             components: 'src/lib/Artist/*.js',
-            ignore: 'src/lib/Artist/index.js',
-            exampleMode: 'hide'
-            // content: 'src/docs/Artist.md'
+            ignore: ['src/lib/Artist/index.js', 'src/lib/Artist/useArtist.js'],
+            exampleMode: 'collapse'
         }
     ]
 };
