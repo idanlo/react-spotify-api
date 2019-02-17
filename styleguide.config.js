9471a1637f2926d766fd9858e41dc07ee092d891
idanlo <idan2110@gmail.com>
2019-02-17 19:38:18 +0200

    add tracks section

--- styleguide.config.js
+++ styleguide.config.js
@@ -9,6 +9,12 @@ module.exports = {
             components: 'src/lib/Artist/*.js',
             ignore: ['src/lib/Artist/index.js', 'src/lib/Artist/useArtist.js'],
             exampleMode: 'collapse'
+        },
+        {
+            name: 'Track components',
+            components: 'src/lib/Track/*.js',
+            ignore: ['src/lib/Track/index.js', 'src/lib/Track/useTrack.js'],
+            exampleMode: 'collapse'
         }
     ]
 };
