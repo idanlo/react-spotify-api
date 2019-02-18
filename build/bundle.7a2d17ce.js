ed312cc8ea795065c49a757568842a61f09cba1e
idanlo <idan2110@gmail.com>
2019-02-18 22:33:45 +0200

    add a lot more documentation :D

--- build/bundle.7a2d17ce.js
+++ build/bundle.7a2d17ce.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	// script path function
 /******/ 	function jsonpScriptSrc(chunkId) {
-/******/ 		return __webpack_require__.p + "build/" + ({}[chunkId]||chunkId) + "." + {"1":"423adc22"}[chunkId] + ".js"
+/******/ 		return __webpack_require__.p + "build/" + ({}[chunkId]||chunkId) + "." + {"1":"4e2ca593"}[chunkId] + ".js"
 /******/ 	}
 /******/
 /******/ 	// The require function
