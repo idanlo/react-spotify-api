6eb34d3aea146eb5e24caf4d5d82686aab723b89
idanlo <idan2110@gmail.com>
2019-02-20 14:51:17 +0200

    add tests for api components and hooks, with jest mocking fetch api

--- src/lib/ApiRequest/ApiRequest.test.js
+++ src/lib/ApiRequest/ApiRequest.test.js
@@ -1,41 +1,55 @@
 import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { mount, shallow } from 'enzyme';
 import ApiRequest from './ApiRequest';
 
-describe('disable tests for now because I changed the ApiRequest component', () => {
-    it('.', () => {
-        expect(true).toBeTruthy();
+describe('ApiRequest', () => {
+    beforeEach(() => {
+        const mockDataPromise = Promise.resolve({
+            test: 1
+        });
+        const mockFetchPromise = Promise.resolve({
+            json: () => mockDataPromise
+        });
+        jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+    });
+
+    afterEach(() => {
+        global.fetch.mockClear();
+    });
+    it('calls props.children initially with parameters (null, false, false)', () => {
+        const fn = jest.fn(() => null);
+        act(() => {
+            mount(<ApiRequest url="https://google.com" children={fn} />);
+        });
+
+        expect(fn).toHaveBeenNthCalledWith(1, null, false, false);
+    });
+
+    it('calls props.children on the second time with parameters (null, true, false)', () => {
+        const fn = jest.fn(() => null);
+        act(() => {
+            mount(<ApiRequest url="https://google.com" children={fn} />);
+        });
+
+        expect(fn).toHaveBeenNthCalledWith(2, null, true, false);
+    });
+
+    it('calls props.children on the third (after receiving data) time with parameters ({test: 1}, false, false)', done => {
+        let wrapper;
+        const fn = jest.fn(() => null);
+        act(() => {
+            wrapper = mount(
+                <ApiRequest url="https://google.com" children={fn} />
+            );
+        });
+        process.nextTick(() => {
+            act(() => {
+                wrapper.update();
+            });
+
+            expect(fn).toHaveBeenLastCalledWith({ test: 1 }, false, false);
+            done();
+        });
     });
 });
-
-// disable tests for now because I changed the ApiRequest component
-
-// describe('<ApiRequest />', () => {
-//   it('should have data given from the axios mock inside state.data', () => {
-//     const children = jest.fn();
-//     const wrapper = shallow(
-//       <ApiRequest
-//         url="https://test.com/ApiRequest"
-//         children={children}
-//         token="tes"
-//       />
-//     );
-//     return Promise.resolve().then(() => {
-//       const state = wrapper.state();
-//       expect(state.data).toHaveProperty('url', 'https://test.com/ApiRequest');
-//     });
-//   });
-//   it('should have the data in state after componentDidMount', () => {
-//     const children = jest.fn();
-//     const wrapper = shallow(
-//       <ApiRequest
-//         url="https://test.com/ApiRequest"
-//         children={children}
-//         token="tes"
-//       />
-//     );
-//     return Promise.resolve().then(() => {
-//       const state = wrapper.state();
-//       expect(children).toHaveBeenCalledWith(state.data, false, false);
-//     });
-//   });
-// });
--- src/lib/ApiRequest/useApiRequest.test.js
+++ src/lib/ApiRequest/useApiRequest.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { act } from 'react-dom/test-utils';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import useApiRequest from './useApiRequest';
+import SpotifyApiContext from '../context';
 
 const Comp = () => {
     const { data, loading, error } = useApiRequest('https://google.com');
@@ -12,54 +13,74 @@ const Comp = () => {
     else return <h1>none</h1>;
 };
 
+const Wrapper = () => (
+    <SpotifyApiContext.Provider value="123">
+        <Comp />
+    </SpotifyApiContext.Provider>
+);
+
 describe('useApiRequest', () => {
-    it('sets loading state property to true', () => {
-        global.fetch = function fetch() {
-            return {
-                then(fn) {
-                    return fn({
-                        json() {
-                            return new Promise((reject, resolve) =>
-                                setTimeout(() => resolve({ test: 1 }), 1000)
-                            );
-                        }
-                    });
-                },
-                catch(fn) {}
-            };
-        };
+    beforeEach(() => {
+        const mockDataPromise = Promise.resolve({
+            test: 1
+        });
+        const mockFetchPromise = Promise.resolve({
+            json: () => mockDataPromise
+        });
+        jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+    });
+
+    afterEach(() => {
+        global.fetch.mockClear();
+    });
+    it('sets loading state property to true on mount', () => {
         let wrapper;
         act(() => {
-            wrapper = mount(<Comp />);
+            wrapper = mount(<Wrapper />);
         });
         expect(wrapper.find('h1').text()).toEqual('loading');
     });
 
-    it('sets data property when finished fetching', () => {
-        global.fetch = function fetch() {
-            return {
-                then(fn) {
-                    return fn({
-                        json() {
-                            return {
-                                then(fn) {
-                                    fn({ test: 1 });
-                                    return {
-                                        catch(fn) {}
-                                    };
-                                },
-                                catch(fn) {}
-                            };
-                        }
-                    });
-                },
-                catch(fn) {}
-            };
-        };
+    it('sets data property when finished fetching', done => {
         let wrapper;
         act(() => {
-            wrapper = mount(<Comp />);
+            wrapper = mount(<Wrapper />);
         });
-        expect(wrapper.find('h1').text()).toEqual('data');
+        process.nextTick(() => {
+            act(() => {
+                wrapper.update();
+            });
+            expect(wrapper.find('h1').text()).toEqual('data');
+            done();
+        });
+    });
+
+    it('calls fetch only one time', () => {
+        act(() => {
+            mount(<Wrapper />);
+        });
+        expect(global.fetch).toHaveBeenCalledTimes(1);
+    });
+
+    it('calls fetch with the correct url', () => {
+        act(() => {
+            mount(<Wrapper />);
+        });
+        expect(global.fetch).toHaveBeenCalledWith(
+            'https://google.com',
+            expect.anything()
+        );
+    });
+
+    it('calls fetch with the correct settings (headers etc..) and correct authorization token ("123" in this test)', () => {
+        act(() => {
+            mount(<Wrapper />);
+        });
+        expect(global.fetch).toHaveBeenCalledWith(expect.anything(), {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer 123'
+            }
+        });
     });
 });
