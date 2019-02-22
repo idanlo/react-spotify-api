85eb62144778d8fc3b500d3e24eb9fc0bd26f910
idanlo <idan2110@gmail.com>
2019-02-22 20:57:08 +0200

    user component does not receive an options object (according to spotify docs)

--- src/lib/User/User.js
+++ src/lib/User/User.js
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ApiRequest from '../ApiRequest/ApiRequest';
 
-const BASE_URL = 'https://api.spotify.com/v1';
-
 /**
  * Get detailed profile information about the current user or a user specified by their Spotify ID (including the current user’s username).<br/>
  * [Response format (current user profile)](https://developer.spotify.com/documentation/web-api/reference/users-profile/get-current-users-profile/#response-format)<br/>
@@ -11,8 +9,8 @@ const BASE_URL = 'https://api.spotify.com/v1';
  * @example ../../docs/User/User.md
  */
 function User(props) {
-    let url = BASE_URL;
-    let options = { ...props.options };
+    let url = 'https://api.spotify.com/v1';
+    let options = {};
     if (props.id) {
         url += `/users/${props.id}`;
     } else {
@@ -29,14 +27,7 @@ function User(props) {
 User.propTypes = {
     /** The Spotify ID of the user (if not specified then current user) */
     id: PropTypes.string,
-    /** Options object (more info above) */
-    options: PropTypes.object,
     /** Process spotify data with render props using props.children as a function */
     children: PropTypes.func.isRequired
 };
-
-User.defaultProps = {
-    options: {}
-};
-
 export default User;
