7e23946fbcb68d7b4a1e46c0e7d48e17e51db5b1
idanlo <idan2110@gmail.com>
2019-06-07 20:21:38 +0300

    add the search query to the dependency list of the useApiRequest hook and also move the loadData function inside the hook to follow the hooks rules

--- src/lib/ApiRequest/useApiRequest.js
+++ src/lib/ApiRequest/useApiRequest.js
@@ -3,35 +3,35 @@ import { serialize } from '../utils';
 import { SpotifyApiContext } from '../';
 
 function useApiRequest(url, options = {}) {
-    const [loading, setLoading] = React.useState(false);
-    const [error, setError] = React.useState(false);
-    const [data, setData] = React.useState(null);
-    const token = React.useContext(SpotifyApiContext);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
+  const [data, setData] = React.useState(null);
+  const token = React.useContext(SpotifyApiContext);
 
-    React.useEffect(() => {
-        loadData();
-    }, [url, options.ids]);
-
+  React.useEffect(() => {
     async function loadData() {
-        try {
-            setLoading(true);
-            const res = await fetch(url + serialize(options), {
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
-            const data = await res.json();
-            console.log(data);
-            setData(data);
-            setLoading(false);
-        } catch (e) {
-            setLoading(false);
-            setError(true);
-        }
+      try {
+        setLoading(true);
+        const res = await fetch(url + serialize(options), {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        const data = await res.json();
+        console.log(data);
+        setData(data);
+        setLoading(false);
+      } catch (e) {
+        setLoading(false);
+        setError(true);
+      }
     }
 
-    return { data, loading, error };
+    loadData();
+  }, [url, options.ids, options.q, options, token]);
+
+  return { data, loading, error };
 }
 
 export default useApiRequest;
