cb06f32f3ed11a82e3183210774bb38093fcdfb7
idanlo <idan2110@gmail.com>
2019-06-09 15:40:24 +0300

    #22 - add more mock paths to return different types of errors

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,23 +4,37 @@ import { configure } from 'enzyme';
 configure({ adapter: new Adapter() });
 
 global.beforeEach(() => {
-    const mockFetchPromise = url => {
-        // throw an error if the url is empty
-        if (url === '') {
-            return Promise.reject({ reason: 'wrong url' });
-        }
-        return Promise.resolve({
-            json: () =>
-                Promise.resolve({
-                    url
-                })
-        });
-    };
-    jest.spyOn(global, 'fetch').mockImplementation(url =>
-        mockFetchPromise(url)
-    );
+  const mockFetchPromise = url => {
+    // throw an error if the url is empty
+    if (url === '') {
+      return Promise.reject({ message: 'wrong url' });
+    } else if (url === 'api.error') {
+      return Promise.resolve({
+        ok: false,
+        json: () =>
+          Promise.resolve({
+            error: {
+              status: 401,
+              message: 'Invalid access token'
+            }
+          })
+      });
+    } else if (url === 'api.not.ok') {
+      return Promise.resolve({
+        ok: false
+      });
+    }
+    return Promise.resolve({
+      ok: true,
+      json: () =>
+        Promise.resolve({
+          url
+        })
+    });
+  };
+  jest.spyOn(global, 'fetch').mockImplementation(url => mockFetchPromise(url));
 });
 
 global.afterEach(() => {
-    global.fetch.mockClear();
+  global.fetch.mockClear();
 });
