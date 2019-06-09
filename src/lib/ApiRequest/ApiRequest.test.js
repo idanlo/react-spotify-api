f0a928547c73c8ad2dfe9186e24862cc9f1265b9
idanlo <idan2110@gmail.com>
2019-06-09 15:41:20 +0300

    #22 - fix older apirequest component tests - the error is now an object/null and not a boolean value

--- src/lib/ApiRequest/ApiRequest.test.js
+++ src/lib/ApiRequest/ApiRequest.test.js
@@ -4,59 +4,58 @@ import { mount } from 'enzyme';
 import ApiRequest from './ApiRequest';
 
 describe('ApiRequest', () => {
-    it('calls props.children initially with parameters (null, false, false)', () => {
-        const fn = jest.fn(() => null);
-        act(() => {
-            mount(<ApiRequest url="https://google.com" children={fn} />);
-        });
-
-        expect(fn).toHaveBeenNthCalledWith(1, null, false, false);
+  it('calls props.children initially with parameters (null, false, false)', () => {
+    const fn = jest.fn(() => null);
+    act(() => {
+      mount(<ApiRequest url="https://google.com" children={fn} />);
     });
 
-    it('calls props.children on the second time with parameters (null, true, false)', () => {
-        const fn = jest.fn(() => null);
-        act(() => {
-            mount(<ApiRequest url="https://google.com" children={fn} />);
-        });
+    expect(fn).toHaveBeenNthCalledWith(1, null, false, null);
+  });
 
-        expect(fn).toHaveBeenNthCalledWith(2, null, true, false);
+  it('calls props.children on the second time with parameters (null, true, false)', () => {
+    const fn = jest.fn(() => null);
+    act(() => {
+      mount(<ApiRequest url="https://google.com" children={fn} />);
     });
 
-    it('calls props.children on the third (after receiving data) time with parameters ({test: 1}, false, false)', done => {
-        let wrapper;
-        const fn = jest.fn(() => null);
-        act(() => {
-            wrapper = mount(
-                <ApiRequest url="https://google.com" children={fn} />
-            );
-        });
-        process.nextTick(() => {
-            act(() => {
-                wrapper.update();
-            });
+    expect(fn).toHaveBeenNthCalledWith(2, null, true, null);
+  });
 
-            expect(fn).toHaveBeenLastCalledWith(
-                { url: 'https://google.com' },
-                false,
-                false
-            );
-            done();
-        });
+  it('calls props.children on the third (after receiving data) time with parameters ({test: 1}, false, false)', done => {
+    let wrapper;
+    const fn = jest.fn(() => null);
+    act(() => {
+      wrapper = mount(<ApiRequest url="https://google.com" children={fn} />);
     });
+    process.nextTick(() => {
+      act(() => {
+        wrapper.update();
+      });
 
-    it('calls props.children with the error parameter set to true when receiving Promise.reject with parameters (null, false, true)', done => {
-        let wrapper;
-        const fn = jest.fn(() => null);
-        act(() => {
-            wrapper = mount(<ApiRequest url="" children={fn} />);
-        });
-        process.nextTick(() => {
-            act(() => {
-                wrapper.update();
-            });
-
-            expect(fn).toHaveBeenLastCalledWith(null, false, true);
-            done();
-        });
+      expect(fn).toHaveBeenLastCalledWith(
+        { url: 'https://google.com' },
+        false,
+        null
+      );
+      done();
     });
+  });
+
+  it('calls props.children with the error parameter set to true when receiving Promise.reject with parameters (null, false, true)', done => {
+    let wrapper;
+    const fn = jest.fn(() => null);
+    act(() => {
+      wrapper = mount(<ApiRequest url="" children={fn} />);
+    });
+    process.nextTick(() => {
+      act(() => {
+        wrapper.update();
+      });
+      expect(fn).toHaveBeenLastCalledWith(null, false, {
+        message: 'wrong url'
+      });
+      done();
+    });
+  });
 });
